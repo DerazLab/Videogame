d5e9378d2ed4d8142c936c94803cf7d92b05b012
Darki-gd <javierisaac2702@gmail.com>
2025-05-11 16:09:19 -0700

    Update GameStateManager.java

--- src/GameState/GameStateManager.java
+++ src/GameState/GameStateManager.java
@@ -1,1 +1,48 @@
+package GameState;
 
+import java.util.ArrayList;
+import java.awt.Graphics2D;
+
+public class GameStateManager
+{
+    private ArrayList<GameState> gameStates;
+    private int currentState;
+
+    public static final int INMENU = 0;
+    public static final int INLEVEL = 1;
+
+    public GameStateManager()
+    {
+        gameStates = new ArrayList<GameState>()
+
+        currentState = INMENU;
+        gameStates.add(new MenuState(this));
+
+    }
+
+    public void setState(int state)
+    {
+        this.currentState = state;
+        gameStates.get(currentState).init();
+    }
+
+    public void update()
+    {
+        gameStates.get(currentState).update();
+    }
+
+    public void draw(Graphics2D g)
+    {
+        gameStates.get(currentState).draw(g);
+    }
+
+    public void keyPressed(int k)
+    {
+        gameStates.get(currentState).keyPressed(k);
+    }
+
+    public void keyReleased(int k)
+    {
+        gameStates.get(currentState).keyReleased(k);       
+    }
+}
