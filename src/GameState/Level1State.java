4aa2dd039ed0c79a4481ae9079016c9afcc5d7e7
Darki-gd <javierisaac2702@gmail.com>
2025-06-02 14:39:47 -0700

    a

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -18,9 +18,19 @@ public class Level1State extends GameState
     public void init() 
     {
         tileMap = new TileMap(30);
+        tileMap.loadTiles("/Tilesets/tileset1.png");
+        tileMap.loadMap("/Maps/level1.map");
+        tileMap.setPosition(0, 0);
     }
+
     public void update() {}
-    public void draw(Graphics2D g) {}
+    public void draw(Graphics2D g) 
+    {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
+
+        tileMap.draw(g);
+    }
     public void keyPressed(int k) {}
     public void keyReleased(int k) {}
 }
