fc52bc969b69e31776110c998ff624ba6d0cc3fd
DL <e2208862@uabc.edu.mx>
2025-06-02 20:51:08 -0700

    Sprites

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -13,5 +13,38 @@ public class Player extends MapObject
     private int maxHealth;
     private int fire;
     private int maxFire;
+    private boolean dead;
+    private boolean flinching;
+    private long flinchTime;
+
+    // fireball
+    private boolean firing;
+    private int fireCost;
+    private int fireBallDamage;
+
+    // private ArrayList<FireBall> fireBalls;
+    private boolean scratching;
+    private int scratchDamage;
+    private int scratchRange;
+
+    // gliding -- lo quitamos 
+    private boolean gliding;
+
+    // animation
+    private ArrayList<BufferedImage[]> sprites;
     
+    //frame para cada animation action
+	private final int[] numFrames = {
+        2, 8, 1, 2, 4, 2, 5
+	};
+
+    // animation actions
+
+	private static final int IDLE = 0;
+	private static final int WALKING = 1;
+	private static final int JUMPING = 2;
+	private static final int FALLING = 3;
+	private static final int GLIDING = 4;
+	private static final int FIREBALL = 5;
+	private static final int SCRATCHING = 6;
 }
