51b46614e4e07e68073446e9b407d8445b3ab5c9
DL <e2208862@uabc.edu.mx>
2025-06-02 17:32:15 -0700

    test

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -19,8 +19,8 @@ public class Level1State extends GameState
     public void init() 
     {
         tileMap = new TileMap(16);
-        tileMap.loadTiles("Tilesets/tileset1.png");
-        tileMap.loadMap("Maps/level1.map");
+        tileMap.loadTiles("/Tilesets/tileset1.png");
+        tileMap.loadMap("/Maps/level1.map");
         tileMap.setPosition(0, 0);
     }
 
