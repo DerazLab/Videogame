1b5f949087b1a659863db0ae5518c79e2d23fafc
DL <e2208862@uabc.edu.mx>
2025-06-03 00:38:23 -0700

    test

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -61,9 +61,9 @@ public class Level1State extends GameState
 		if(k == KeyEvent.VK_UP) player.setUp(true);
 		if(k == KeyEvent.VK_DOWN) player.setDown(true);
 		if(k == KeyEvent.VK_W) player.setJumping(true);
-		if(k == KeyEvent.VK_E) player.setGliding(true);
-		if(k == KeyEvent.VK_R) player.setScratching();
-		if(k == KeyEvent.VK_F) player.setFiring();
+		//if(k == KeyEvent.VK_E) player.setGliding(true);
+		//if(k == KeyEvent.VK_R) player.setScratching();
+		//if(k == KeyEvent.VK_F) player.setFiring();
 	}
 	
 	public void keyReleased(int k) {
@@ -72,7 +72,7 @@ public class Level1State extends GameState
 		if(k == KeyEvent.VK_UP) player.setUp(false);
 		if(k == KeyEvent.VK_DOWN) player.setDown(false);
 		if(k == KeyEvent.VK_W) player.setJumping(false);
-		if(k == KeyEvent.VK_E) player.setGliding(false);
+		//if(k == KeyEvent.VK_E) player.setGliding(false);
 	}
 
 }
