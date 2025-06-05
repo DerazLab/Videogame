ed2b0357dc086e0f2959286cca108cda38b96964
Darki-gd <javierisaac2702@gmail.com>
2025-06-04 21:25:27 -0700

    a

---
+++ src/Entity/Flagpole.java
@@ -0,0 +1,41 @@
+package Entity;
+
+import TileMap.TileMap;
+import java.awt.Rectangle;
+
+public class Flagpole extends MapObject {
+    private boolean active;
+
+    public Flagpole(TileMap tm, double x, double y) {
+        super(tm);
+        this.x = x;
+        this.y = y;
+        width = 16;
+        height = 48; // Assuming flagpole spans multiple tiles vertically
+        cwidth = 8;
+        cheight = 48;
+        active = true;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public void draw(java.awt.Graphics2D g) {
+        // Drawing handled by TileMap, so no need to implement
+    }
+
+    public Rectangle getRectangle() {
+        return new Rectangle(
+            (int)(x - cwidth / 2),
+            (int)(y - cheight / 2),
+            cwidth,
+            cheight
+        );
+    }
+}
